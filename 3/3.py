ac4e9afed717105d924d365b310cccaedf79e855
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-08-20 17:45:25 +0530

    add caching to #3

--- 3/3.py
+++ 3/3.py
@@ -1,12 +1,13 @@
+from functools import lru_cache
 from math import sqrt
 
 num = 600851475143
 
-
+@lru_cache(maxsize=None)
 def find_prime_cofactors(n):
     for i in range(int(sqrt(n)), 1, -1):
         if n % i == 0:
-            return {*find_prime_cofactors(i), *find_prime_cofactors(n / i)}
+            return {*find_prime_cofactors(i), *find_prime_cofactors(int(n / i))}
     return {n}
 
 
