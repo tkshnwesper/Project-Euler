f750fadc85dbd87b59de4f7bb685fa9e8d231c08
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-08-20 16:56:26 +0530

    refactor #2 remove global sum

--- 2/2.py
+++ 2/2.py
@@ -1,17 +1,11 @@
-sum = 0
-
-
 def fib(a=0, b=1):
-    global sum
     a, b = b, a + b
     if a % 2 == 0:
-        sum += a
+        return fib(a, b) + a
     if b >= 40_00_000:
-        return
-    fib(a, b)
+        return 0
+    return fib(a, b)
 
 
-fib()
-
-print(sum)
+print(fib())
 
